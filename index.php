95edb6d79fe153e0a83ff059bfdf6b309c5bf14f
Alex <alexbar137@gmail.com>
2014-08-04 21:24:32 +0700

    Folders

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html" charset=utf-8">
+	<title>
+		_%page_title%_
+	</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="scripts/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="scripts/app.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
